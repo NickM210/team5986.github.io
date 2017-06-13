d3ad4276f23753fc9417395862257ce8ff7c3d48
NickM210 <nmunoz5@mail.stmarytx.edu>
2017-06-13 08:55:25 -0500

    Robotics

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("foot01").innerHTML =
+ new Date().getFullYear() + " Central Catholic High School</p>";
+
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='members.html'>Members</a></li>" +
+"<li><a href='about.html'>About</a></li>" +
+"</ul>";
